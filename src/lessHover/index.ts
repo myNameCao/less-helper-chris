c9d2bb5b22d298222bd4f12739cd9089c87cb089
hefei.cao <hefei.cao@xiaoyangedu.com>
2023-02-22 19:01:42 +0800

    chore: 优化一下样式

--- src/lessHover/index.ts
+++ src/lessHover/index.ts
@@ -39,9 +39,8 @@ const provideHover = async (
 
      }
    
-    const markdown = new vscode.MarkdownString(`<span style="color:${color};background-color:#000;">
-    &nbsp; ${word} :  ${lastColor}    &nbsp;&nbsp;&nbsp;
-     </span>`);
+    const markdown = new vscode.MarkdownString( `${word} : &nbsp;<span style="color:${color};background-color:#000;">
+    &nbsp;&nbsp;&nbsp; ${lastColor} &nbsp;&nbsp;&nbsp;</span> `);
     markdown.isTrusted = true;
     return new vscode.Hover(markdown);
   }
